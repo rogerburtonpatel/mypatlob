81d3108b89d3d587d08fda48545d412a35ee290f
Shikhar2929 <66570303+Shikhar2929@users.noreply.github.com>
2023-07-25 17:52:38 -0400

    Add files via upload
    Testsss

---
+++ Conditionals.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+c = input()
+if a == b:
+    print("A is B")
+elif b != c:
+    print("B is not C")
+elif a == c:
+    print("A is C")
+else:
+    print("No equivalence")
---
+++ ForLoops.py
@@ -0,0 +1,6 @@
+for i in range(15):
+    print(i)
+for i in range(1, 15)
+    print(i)
+for i in range(1, 15, 2)
+    print(i)
